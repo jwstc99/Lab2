2e5c75649eddd3743719afbc81bc39fd52cfb0db
Jack Williams <jack@stcwilliams.com>
2018-01-30 16:21:49 -0600

    Created Random Step Solver

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -1,4 +1,5 @@
 import edu.illinois.cs.cs125.lib.mazemaker.Maze;
+import java.lang.Math;
 
 /**
  * Solve a randomly-generated maze.
@@ -33,10 +34,51 @@ public class SolveMaze {
          * You should be able to solve a 10 x 10 maze in (far fewer than) 1000 steps.
          * Feel free to adjust this number if you experiment with other mazes.
          */
-        for (int step = 0; step < 1000; step++) {
+        for (int step = 0; step < 100000; step++) {
             // Implement your maze solving algorithm here
+            System.out.println(step);
+            if (maze.isFinished()) {
+                break;
+            }
+            /* maze.turnRight();
+            if (maze.canMove()) {
+                maze.move();
+                continue;
+            } else {
+                maze.turnLeft();
+            }
+            if (maze.canMove()) {
+                maze.move();
+                continue;
+            } else {
+                maze.turnLeft();
+            }
+            if (maze.canMove()) {
+                maze.move();
+                continue;
+            } else {
+                maze.turnLeft();
+                maze.move();
+            }
+        }*/
+            int random = ((int) (Math.random() * 100)) % 4;
+            if (random == 0) {
+                maze.move();
+            }
+            if (random == 1) {
+                maze.turnLeft();
+                maze.move();
+            }
+            if (random == 2) {
+                maze.turnRight();
+                maze.move();
+            }
+            if (random == 3) {
+                maze.turnRight();
+                maze.turnRight();
+                maze.move();
+            }
         }
-
         if (maze.isFinished()) {
             System.out.println("You solved the maze!");
         } else {
